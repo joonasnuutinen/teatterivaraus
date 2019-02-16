89a577eb7722aa9bcf1d24f9f1c9775902f1b1ec
Joonas Nuutinen <joonas@joonasnuutinen.com>
2019-02-16 12:43:07 +0200

    Change error page layout

--- app.js
+++ app.js
@@ -123,7 +123,7 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render('error', { theatre: req.user });
 });
 
 module.exports = app;
